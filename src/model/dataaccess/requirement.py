c338ab5c5362ff7d6d65813c201c2e9f41ba6cad
Diego Beraldin <diego.beraldin@gmail.com>
2013-11-09 13:46:45 +0100

    documented module dataaccess.requirements

--- src/model/dataaccess/requirement.py
+++ src/model/dataaccess/requirement.py
@@ -4,11 +4,14 @@
 """
 
 from src.model import database as db
+from src.model.constants import PRIORITY_LIST, TYPE_LIST
 from src.model.mapping import UseCase, Requirement, Source, SystemTest
 
 
 def create_requirement(req_id, description, req_type, priority, source_id,
         parent_id=None):
+    """Creates a new requirement with the information provided.
+    """
     if parent_id and not _is_requirement_existing(parent_id):
         raise Exception('Nonexistent parent')
     with db.get_session() as session:
@@ -18,6 +21,8 @@ def create_requirement(req_id, description, req_type, priority, source_id,
 
 
 def delete_requirement(req_id):
+    """Deletes the requirement with the given ID.
+    """
     with db.get_session() as session:
         session.query(Requirement).filter(
                 Requirement.req_id == req_id).delete()
@@ -30,11 +35,16 @@ def delete_requirement(req_id):
 
 
 def get_all_requirement_ids():
+    """Extracts the IDs of all the requirements that have been saved.
+    """
     with db.get_session() as session:
         return [r[0] for r in session.query(Requirement.req_id)]
 
 
 def get_all_requirement_names_and_descriptions():
+    """Returns a list of dictionaries containing the 'id' and 'description'
+    keys corresponding to all the requirements that have been created.
+    """
     with db.get_session() as session:
         return [{'id': req[0], 'description': req[1]}
                 for req in
@@ -42,6 +52,8 @@ def get_all_requirement_names_and_descriptions():
 
 
 def get_requirement(req_id):
+    """Converts a requirement ID to the given transfer object.
+    """
     with db.get_session() as session:
         requirement = session.query(Requirement).filter(
                 Requirement.req_id == req_id).scalar()
@@ -50,6 +62,9 @@ def get_requirement(req_id):
 
 
 def get_requirement_children_ids(req_id):
+    """Returns a list of IDs corresponding to the children of the requirement
+    with the given ID.
+    """
     with db.get_session() as session:
         return [req[0] for req in
                 session.query(Requirement.req_id).filter(
@@ -57,6 +72,9 @@ def get_requirement_children_ids(req_id):
 
 
 def get_top_level_requirement_ids():
+    """Returns the list of all those IDs that correspond to top-level
+    requirements, i.e. requirements having no parent.
+    """
     with db.get_session() as session:
         return [req[0] for req in
                 session.query(Requirement.req_id).filter(
@@ -64,6 +82,9 @@ def get_top_level_requirement_ids():
 
 
 def _is_requirement_existing(req_id):
+    """Returns True if a requirement with the given ID has already been created
+    and False otherwise.
+    """
     with db.get_session() as session:
         count = session.query(Requirement).filter(
                  Requirement.req_id == req_id).count()
@@ -72,6 +93,13 @@ def _is_requirement_existing(req_id):
 
 def update_requirement_associations(req_id, newly_associated_use_cases,
         newly_associated_tests):
+    """Updates the associations between use case and requirement as well as
+    tests and requirement starting from the ID of the base requirement and a
+    list of IDs of the use cases involved and of the test involved.
+    It determines which entries must be added to and which must be removed from
+    either association table (tests-requirement or test-use cases) and updates
+    its content accordingly.
+    """
     with db.get_session() as session:
         requirement = session.query(Requirement).filter(
                 Requirement.req_id == req_id).one()
@@ -102,6 +130,8 @@ def update_requirement_associations(req_id, newly_associated_use_cases,
 
 
 def update_requirement_description(req_id, description):
+    """Updates the description of the requirement with the given ID.
+    """
     with db.get_session() as session:
         requirement = session.query(Requirement).filter(
                 Requirement.req_id == req_id).one()
@@ -109,6 +139,9 @@ def update_requirement_description(req_id, description):
 
 
 def update_requirement_id(req_id, new_req_id):
+    """Updates the ID of the requirement with the given ID, reflecting the
+    change in the association tables if needed.
+    """
     with db.get_session() as session:
         requirement = session.query(Requirement).filter(
                 Requirement.req_id == req_id).one()
@@ -125,6 +158,8 @@ def update_requirement_id(req_id, new_req_id):
 
 
 def update_requirement_parent_id(req_id, parent_id):
+    """Assigns a new parent to the requirement with the given ID.
+    """
     with db.get_session() as session:
         requirement = session.query(Requirement).filter(
                 Requirement.req_id == req_id).one()
@@ -132,6 +167,9 @@ def update_requirement_parent_id(req_id, parent_id):
 
 
 def update_requirement_priority(req_id, priority):
+    """Updates the priority of the requirement with the given ID.
+    """
+    assert priority in PRIORITY_LIST
     with db.get_session() as session:
         requirement = session.query(Requirement).filter(
                 Requirement.req_id == req_id).one()
@@ -139,6 +177,8 @@ def update_requirement_priority(req_id, priority):
 
 
 def update_requirement_source(req_id, source_name):
+    """Updates the source of the requirement with the given ID.
+    """
     with db.get_session() as session:
         requirement = session.query(Requirement).filter(
                 Requirement.req_id == req_id).one()
@@ -147,6 +187,9 @@ def update_requirement_source(req_id, source_name):
 
 
 def update_requirement_type(req_id, req_type):
+    """Updates the type of the requirement with the given ID.
+    """
+    assert req_type in TYPE_LIST
     with db.get_session() as session:
         requirement = session.query(Requirement).filter(
                 Requirement.req_id == req_id).one()
